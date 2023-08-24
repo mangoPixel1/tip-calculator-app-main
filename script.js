781cd699243954c6f0658efdd42575b8aeeb7192
Miguel Cavada <70104762+mangoPixel1@users.noreply.github.com>
2023-08-24 09:52:19 -0700

    Error messages and Reset button complete

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 function calculateTip (tipPercent) {
-    let billAmount = document.querySelector('#billInput').value;
-    let numPeople = document.querySelector('#numPeopleInput').value;
-
-    let bill = parseInt(billAmount);
-    let people = parseInt(numPeople);
-    let tip = (parseInt(tipPercent) / 100);
+    document.querySelector('.reset-button').classList.add('reset-enabled');
     
+    document.querySelector('.billInput').classList.remove('error-input');
+    document.querySelector('.numPeopleInput').classList.remove('error-input');
+    document.querySelector('#bill-title > p').classList.remove('visible');
+    document.querySelector('#numPeople-title > p').classList.remove('visible');
+    
+    let billAmount = document.querySelector('.billInput').value;
+    let numPeople = document.querySelector('.numPeopleInput').value;
 
-    /*if (billAmount.length > 0 && numPeople.length > 0) {
-        bill = parseInt(billAmount);
-        people = parseInt(numPeople);
-    }*/
+    if (isNaN(parseFloat(billAmount)) || isNaN(parseFloat(numPeople))) {
+        if (isNaN(parseFloat(billAmount))) {
+            // alert("Bill can't be zero");
+            document.querySelector('.billInput').classList.add('error-input');
+            document.querySelector('#bill-title > p').classList.add('visible');
+        }
+        
+        if (isNaN(parseFloat(numPeople))) {
+            //alert("Number of People can't be zero");
+            document.querySelector('.numPeopleInput').classList.add('error-input');
+            document.querySelector('#numPeople-title > p').classList.add('visible');
+        }
+        return;
+    }
 
-    const totalTip = bill * tip; // (bill * tip).toFixed(2);
+    let bill = parseFloat(billAmount);
+    let people = parseFloat(numPeople);
+    let tip = (parseFloat(tipPercent) / 100);
+
+    const totalTip = bill * tip;
     const totalAmount = bill + totalTip;
+
+    if (isNaN(totalTip) || isNaN(totalAmount)) {
+        return;
+    }
 
     document.querySelector('#tip-per-person').textContent = "$" + `${(totalTip / people).toFixed(2)}`;
     document.querySelector('#total-per-person').textContent = "$" + `${(totalAmount / people).toFixed(2)}`;
 
-    document.querySelector('.reset-button').classList.add('reset-enabled');
+    //document.querySelector('.reset-button').classList.add('reset-enabled');
 }
 
 const tipButtons = document.querySelectorAll('.tip-button');
@@ -41,15 +61,21 @@ tipButtons.forEach(button => {
     });
 });
 
+// Reset all values when reset button is clicked
 document.querySelector('.reset-button').addEventListener('click', () => {
-    document.querySelector('#billInput').value = null;
-    document.querySelector('#numPeopleInput').value = null;
+    document.querySelector('.billInput').value = null;
+    document.querySelector('.numPeopleInput').value = null;
     
     tipButtons.forEach(tipButton => { // Remove the selected class from currently selected button
         if (tipButton.classList.contains('selected')) {
             tipButton.classList.remove('selected');
         }
     });
+
+    document.querySelector('.billInput').classList.remove('error-input');
+    document.querySelector('.numPeopleInput').classList.remove('error-input');
+    document.querySelector('#bill-title > p').classList.remove('visible');
+    document.querySelector('#numPeople-title > p').classList.remove('visible');
 
     document.querySelector('#tip-per-person').textContent = "$0.00";
     document.querySelector('#total-per-person').textContent = "$0.00";
